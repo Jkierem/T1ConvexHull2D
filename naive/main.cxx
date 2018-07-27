937922e2a677ca39baed638e81d10c50c6f27462
jkierem <juanmig8@hotmail.com>
2018-07-26 19:30:34 -0500

    Initial commit

---
+++ naive/main.cxx
@@ -0,0 +1,9 @@
+#include <CGAL/Simple_cartesian.h>
+
+typedef CGAL::Simple_cartesian<int> Kernel;
+typedef Kernel::Point_2 Point_2;
+typedef Kernel::Segment_2 Segment_2;
+
+int main(){
+	return 0;
+}
